05036f3163a4357fc68d27031f68eb19af9921cb
Rodrigo Klaes <rodrigoklaes@gmail.com>
2021-09-19 22:48:16 -0300

    feat: finish hobbie crud

--- src/app/hobbies/hobbie.controller.ts
+++ src/app/hobbies/hobbie.controller.ts
@@ -18,10 +18,40 @@ export default class HobbieController {
   create = async (req: Request, res: Response): Promise<Response> => {
     try {
       const response = await this.hobbieService.create(req.body);
+      return res.status(201).json(response);
+    } catch (err: any) {
+      logger.error(err);
+      return res.status(err.status_code || 500);
+    }
+  };
+
+  get = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const response = await this.hobbieService.get(req.params.id);
       return res.json(response);
     } catch (err: any) {
       logger.error(err);
       return res.status(err.status_code || 500);
+    }
+  };
+
+  update = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      await this.hobbieService.update(req.body, req.params.id);
+      return res.status(204).json();
+    } catch (err: any) {
+      logger.error(err);
+      return res.status(err.status_code || 500);
+    }
+  };
+
+  delete = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      await this.hobbieService.delete(req.params.id);
+      return res.status(204).json();
+    } catch (err: any) {
+      logger.error(err);
+      return res.status(500).json(err);
     }
   };
 }
--- src/app/hobbies/routes.ts
+++ src/app/hobbies/routes.ts
@@ -5,6 +5,9 @@ const routes = Router();
 const hobbieController = makeHobbieController();
 
 routes.get('/', hobbieController.list);
+routes.get('/:id', hobbieController.get);
 routes.post('/', hobbieController.create);
+routes.put('/:id', hobbieController.update);
+routes.delete('/:id', hobbieController.delete);
 
 export default routes;
